15b1db9bd40caa6cdd280684175ee8b8d7d5d0a6
Dave Gumba <dave_gumba@Yahoo.com>
2017-12-31 23:43:42 -0500

    fixed a logout error

--- mount/Controller/WelcomeViewController.swift
+++ mount/Controller/WelcomeViewController.swift
@@ -7,9 +7,8 @@
 //
 
 import UIKit
-import Firebase
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
