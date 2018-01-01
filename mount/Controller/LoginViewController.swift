6b9496ce44306d513a2fb6a09624a8eea8a5a195
Dave Gumba <dave_gumba@Yahoo.com>
2018-01-01 00:16:20 -0500

    done

--- mount/Controller/LoginViewController.swift
+++ mount/Controller/LoginViewController.swift
@@ -13,6 +13,7 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,8 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginPressed(_ sender: Any) {
+        
+        loginButton.isEnabled = false
         
         let username = usernameTextField.text ?? ""
         let password = passwordTextField.text ?? ""
